2cd5c281e94292fe874a22d941ebf64e1a42bfde
Kaizodo Technologies Pvt Ltd <developers@kaizodo.com>
2024-01-26 22:35:54 +0530

    upgrading angular version

--- projects/ngx-datepicker/src/public-api.ts
+++ projects/ngx-datepicker/src/public-api.ts
@@ -1,7 +1,3 @@
-/*
- * Public API Surface of ngx-datepicker
- */
-
 export * from './lib/ngx-datepicker.service';
 export * from './lib/datepicker/datepicker.component';
 export * from './lib/ngx-datepicker.directive';
